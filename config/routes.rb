9fadd51f458254009bbad004e67eaf71a1c2b0e6
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2015-08-18 18:21:33 +0200

    Support more than two hardcoded languages. Previously, all supported languages were hardcoded in three locations (routes, header partial, each individual locale file) which wasn't very elegant. Now, only I18n.available_locales has to be modified in config/application.rb. Thias has an effect on the accepted routes, as well as the frontend which will provide a switcher with all available locales (pulling its name from the locale definition, if it exists).

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Dmptool2::Application.routes.draw do
-  scope "(:locale)", locale: /en|de/ do
-
+  # Have an optional locale specifier as the first URL component.
+  # All locales known to I18n, which can be whitelisted in
+  # config/application.rb, are allowed.
+  scope "(:locale)",
+        constraints: lambda {|r| !r.params[:locale] || I18n.available_locales.include?(r.params[:locale].to_sym)} do
     namespace :api, :defaults => {:format => :json} do
       namespace :v1 do
         resources :resources
@@ -16,26 +19,26 @@ Dmptool2::Application.routes.draw do
         resources :published_plans
         resources :plans_states
         resources :sample_plans
-  
+
         get 'institutions_plans_count/:id', to: 'institutions#plans_count_show'
         get 'institutions_plans_count', to: 'institutions#plans_count_index'
         get 'institutions_admins_count/:id', to: 'institutions#admins_count_show'
         get 'institutions_admins_count', to: 'institutions#admins_count_index'
-  
+
         get 'plans_full/:id', to: 'plans#plans_full_show'
         get 'plans_full', to: 'plans#plans_full_index'
-  
+
       end
     end
-  
-  
+
+
     match 'remove_token', to: 'users#remove_token', :via => [:get, :post]
     match 'add_token', to: 'users#add_token', :via => [:get, :post]
-  
+
     match '/500', :to => 'errors#internal_error', :via => :all
     match '/404', :to => 'errors#not_found', :via => :all
     match '/403', :to => 'errors#forbidden', :via => :all
-  
+
     get "dashboard/show"
     root 'static_pages#home'
     get 'about', to: 'static_pages#about'
@@ -59,9 +62,9 @@ Dmptool2::Application.routes.draw do
     post 'requirements/reorder', to: 'requirements#reorder', as: "reorder_requirements"
     get 'customizations/:id/requirements/(:requirement_id)', to: 'customizations#show', as: 'customization_requirement'
     get 'institutions', to: 'institutions#index'
-  
-    
-  
+
+
+
     resources :requirements_templates do
       resources :requirements
       resources :resource_contexts
@@ -72,10 +75,10 @@ Dmptool2::Application.routes.draw do
       resources :sample_plans
       resources :additonal_informations
     end
-  
+
     resources :resources
     resources :resource_contexts
-  
+
     #this routes needs to stay above the other user ones to take priority
     get 'users/autocomplate_users', to: 'users#autocomplete_users', as: 'users_autocomplete'
     get 'users/autocomplate_users_plans', to: 'users#autocomplate_users_plans', as: 'autocomplate_users_plans'
@@ -87,11 +90,11 @@ Dmptool2::Application.routes.draw do
         patch :finish_signup_update
       end
     end
-  
+
     # resources :resources do
     #   resources :resource_contexts
     # end
-  
+
     resources :plans do
       post "plan_states/approved"
       post "plan_states/rejected"
@@ -109,10 +112,10 @@ Dmptool2::Application.routes.draw do
     end
     resources :comments
     resources :responses
-  
+
     resources :institutions
     resources :authorizations
-  
+
     match 'user_sessions/login', to: 'user_sessions#login', as: 'login', :via => [:get, :post]
     get 'user_sessions/institution', to: 'user_sessions#institution', as: 'choose_institution'
     match '/auth/:provider/callback', to: 'user_sessions#create', :via => [:get, :post]
@@ -122,7 +125,7 @@ Dmptool2::Application.routes.draw do
     match 'user_sessions/complete_password_reset', to: 'user_sessions#complete_password_reset', as: 'complete_password_reset', :via => [:get, :post]
     get 'requirements_template_information', to: 'requirements_templates#template_information'
     post 'copy_existing_requirements_template', to: 'requirements_templates#copy_existing_template'
-  
+
     get 'plan_template_information', to: 'plans#template_information'
     get 'copy_existing_plans_template', to: 'plans#copy_existing_template'
     get 'select_dmp_template', to: 'plans#select_dmp_template'
@@ -132,7 +135,7 @@ Dmptool2::Application.routes.draw do
     #post 'add_coowner_autocomplete', to: 'plans#add_coowner_autocomplete' #this isn't used and is a helping function for the users controller
     delete 'delete_coowner', to: 'plans#delete_coowner'
     post 'reject_with_comments', to: 'plan_states#reject_with_comments'
-  
+
     get  'remove_resource_editor_role', to: 'resource_templates#remove_resource_editor_role'
     get  'remove_requirements_editor_role', to: 'requirements_templates#remove_requirements_editor_role'
     post 'add_role', to: 'resource_templates#add_role'
@@ -144,53 +147,53 @@ Dmptool2::Application.routes.draw do
     post  'update_user_roles', to: 'users#update_user_roles'
     get  'edit_user_roles_inst_admin', to: 'institutions#edit_user_roles_inst_admin'
     post  'update_user_roles_inst_admin', to: 'institutions#update_user_roles_inst_admin'
-  
+
     post  'add_authorization_manage_users', to: 'authorizations#add_authorization_manage_users'
-  
+
     get 'edit_customization_resource', to: 'resources#edit_customization_resource'
     patch 'update_customization_resource', to: 'resources#update_customization_resource'
-  
+
     post 'new_customization_resource', to: 'resources#new_customization_resource'
     get 'new_customization_resource', to: 'resources#new_customization_resource'
     post 'create_customization_resource', to: 'resources#create_customization_resource'
-  
+
     delete 'delete_resource', to: 'resources#destroy'
-  
+
     post 'select_resource', to: 'resource_contexts#select_resource'
     get 'select_resource', to: 'resource_contexts#select_resource'
-  
+
     delete 'delete_resource_context', to: 'resource_contexts#destroy'
-  
+
     post 'create_customization_resource_from_select_resource', to: 'resources#create_customization_resource_from_select_resource'
-  
+
     post 'copy_selected_customization_resource', to: 'resources#copy_selected_customization_resource'
-  
+
     post 'unlink_resource_from_template', to: 'resource_contexts#unlink_resource_from_template'
     post 'unlink_resource_from_requirement', to: 'resource_contexts#unlink_resource_from_requirement'
     post 'unlink_resource_from_customization', to: 'resource_contexts#unlink_resource_from_customization'
-  
-  
+
+
     post 'unlink_institutional_resource', to: 'resource_contexts#unlink_institutional_resource'
-  
+
     match 'remove_orcid', to: 'users#remove_orcid', :via => [:get, :post]
-  
-  
-  
+
+
+
     # The priority is based upon order of creation: first created -> highest priority.
     # See how all your routes lay out with "rake routes".
-  
+
     # You can have the root of your site routed with "root"
     # root 'welcome#index'
-  
+
     # Example of regular route:
     #   get 'products/:id' => 'catalog#view'
-  
+
     # Example of named route that can be invoked with purchase_url(id: product.id)
     #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-  
+
     # Example resource route (maps HTTP verbs to controller actions automatically):
     #   resources :products
-  
+
     # Example resource route with options:
     #   resources :products do
     #     member do
@@ -202,13 +205,13 @@ Dmptool2::Application.routes.draw do
     #       get 'sold'
     #     end
     #   end
-  
+
     # Example resource route with sub-resources:
     #   resources :products do
     #     resources :comments, :sales
     #     resource :seller
     #   end
-  
+
     # Example resource route with more complex sub-resources:
     #   resources :products do
     #     resources :comments
@@ -216,7 +219,7 @@ Dmptool2::Application.routes.draw do
     #       get 'recent', on: :collection
     #     end
     #   end
-  
+
     # Example resource route within a namespace:
     #   namespace :admin do
     #     # Directs /admin/products/* to Admin::ProductsController
