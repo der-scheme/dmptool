e903db1beb1f0bf09b0b593e7ed86701aafafbb7
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2015-08-19 17:58:34 +0200

    Added translation to the toggle_active view which provides an active/inactive switch for the DMP template status in /requirements_templates/<id>/requirements.

--- app/helpers/requirements_templates_helper.rb
+++ app/helpers/requirements_templates_helper.rb
@@ -1,9 +1,9 @@
 module RequirementsTemplatesHelper
   def activate_link_text(requirements_template)
-    requirements_template.active? ? 'Deactivate' : 'Activate'
+    requirements_template.active? ? t('.deactivate') : t('.activate')
   end
 	def display_status_text(requirements_template)
-    requirements_template.active? ? 'Active' : 'Inactive'
+    requirements_template.active? ? t('.active') : t('.inactive')
   end
   def referer_action
     @referer_url = Rails.application.routes.recognize_path(URI((session[:page_history].blank? ? "": session[:page_history][0])).path)
--- app/helpers/resource_templates_helper.rb
+++ app/helpers/resource_templates_helper.rb
@@ -1,8 +1,8 @@
 module ResourceTemplatesHelper
   def activate_link_text(resource_template)
-    resource_template.active? ? 'Deactivate' : 'Activate'
+    resource_template.active? ? t('.deactivate') : t('.activate')
   end
 	def display_status_text(resource_template)
-    resource_template.active? ? 'Active' : 'Inactive'
+    resource_template.active? ? t('.active') : t('.inactive')
   end
 end
