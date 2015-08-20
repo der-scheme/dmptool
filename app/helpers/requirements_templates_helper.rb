c032b6106028a270d9fa30f2579085ad7e65b96c
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2015-08-20 17:40:54 +0200

    Moved the global template language definitions from "globals.helpers" to "globals".

--- app/helpers/requirements_templates_helper.rb
+++ app/helpers/requirements_templates_helper.rb
@@ -1,13 +1,13 @@
 module RequirementsTemplatesHelper
   def activate_link_text(requirements_template)
     requirements_template.active? ?
-        t('globals.helpers.template.details.action_deactivate') :
-        t('globals.helpers.template.details.action_activate')
+        t('globals.template.details.action_deactivate') :
+        t('globals.template.details.action_activate')
   end
 	def display_status_text(requirements_template)
     requirements_template.active? ?
-        t('globals.helpers.template.details.status_active') :
-        t('globals.helpers.template.details.status_inactive')
+        t('globals.template.details.status_active') :
+        t('globals.template.details.status_inactive')
   end
   def referer_action
     @referer_url = Rails.application.routes.recognize_path(URI((session[:page_history].blank? ? "": session[:page_history][0])).path)
--- app/helpers/resource_templates_helper.rb
+++ app/helpers/resource_templates_helper.rb
@@ -1,12 +1,12 @@
 module ResourceTemplatesHelper
   def activate_link_text(resource_template)
     resource_template.active? ?
-        t('globals.helpers.template.details.action_deactivate') :
-        t('globals.helpers.template.details.action_activate')
+        t('globals.template.details.action_deactivate') :
+        t('globals.template.details.action_activate')
   end
 	def display_status_text(resource_template)
     resource_template.active? ?
-        t('globals.helpers.template.details.status_active') :
-        t('globals.helpers.template.details.status_inactive')
+        t('globals.template.details.status_active') :
+        t('globals.template.details.status_inactive')
   end
 end
