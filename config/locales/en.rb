da8d60bb6ea9349a151441c4fe7afc4d68f5f189
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2015-09-02 14:52:16 +0200

    Make some institutions/form localizations self-sufficient.
    Those tooltip translations which relied on the email parameter can now be found in en.rb. The parameter is now inferred by the translations themselves (relying on APP_CONFIG) and thus was removed from the partial.

--- config/locales/en.rb
+++ config/locales/en.rb
@@ -1,4 +1,11 @@
 {
   en: {
+    institutions: {
+      form: {
+        shib_entity_id_tooltip: "Shibboleth endpoint registered with the Incommon Federation. This can only be edited by the DMPTool Administrator. Contact #{APP_CONFIG['feedback_email_to'].join(', ').gsub(/(.*),(.*)/, '\1 or\2')} with any questions.",
+        shib_domain_id_tooltip: "This can only be edited by the DMPTool Administrator. Contact #{APP_CONFIG['feedback_email_to'].join(', ').gsub(/(.*),(.*)/, '\1 or\2')} with any questions.",
+        parent_tooltip: "If you do not see your institution listed in the dropdown list, contact #{APP_CONFIG['feedback_email_to'].join(', ').gsub(/(.*),(.*)/, '\1 or\2')}."
+      }
+    }
   }
 }
