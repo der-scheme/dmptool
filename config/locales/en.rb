5cf3ad11b0849e2082774fb38781b5ce54d934c4
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2015-09-02 15:12:41 +0200

    Make customization heading in resource_contexts/customization_resources_list dynamic.
    Moved from en.yml to en.rb and merged into one. Previously we had to use to, as
    the translation was dependent on the presence of the institution parameter. With
    a lambda translation we can do this in one key.

--- config/locales/en.rb
+++ config/locales/en.rb
@@ -6,6 +6,21 @@
         shib_domain_id_tooltip: "This can only be edited by the DMPTool Administrator. Contact #{APP_CONFIG['feedback_email_to'].join(', ').gsub(/(.*),(.*)/, '\1 or\2')} with any questions.",
         parent_tooltip: "If you do not see your institution listed in the dropdown list, contact #{APP_CONFIG['feedback_email_to'].join(', ').gsub(/(.*),(.*)/, '\1 or\2')}."
       }
+    },
+    resource_contexts: {
+      customization_resources_list: {
+        resources_customized_for:
+          lambda do |key,
+                     template: fail(ArgumentError, 'template missing'),
+                     institution: nil,
+                     **options|
+            if institution
+              "Template Resources for #{template}  - Customized for %{institution}"
+            else
+              "Template Resources for #{template}  - Customized for all Institutions"
+            end
+          end
+      }
     }
   }
 }
