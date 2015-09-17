326de30a73110ec068bc3562dafbdbe62dac9c81
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2015-09-17 17:17:42 +0200

    Add a function that does that constant array joining for us.

--- config/locales/en.rb
+++ config/locales/en.rb
@@ -1,3 +1,7 @@
+def i18n_join(array)
+  array.join(', ').gsub(/(.*),(.*)/, '\1 or\2')
+end
+
 {
   en: {
     activerecord: {
@@ -26,9 +30,9 @@
     },
     institutions: {
       form: {
-        shib_entity_id_tooltip: "Shibboleth endpoint registered with the Incommon Federation. This can only be edited by the DMPTool Administrator. Contact #{APP_CONFIG['feedback_email_to'].join(', ').gsub(/(.*),(.*)/, '\1 or\2')} with any questions.",
-        shib_domain_id_tooltip: "This can only be edited by the DMPTool Administrator. Contact #{APP_CONFIG['feedback_email_to'].join(', ').gsub(/(.*),(.*)/, '\1 or\2')} with any questions.",
-        parent_tooltip: "If you do not see your institution listed in the dropdown list, contact #{APP_CONFIG['feedback_email_to'].join(', ').gsub(/(.*),(.*)/, '\1 or\2')}."
+        shib_entity_id_tooltip: "Shibboleth endpoint registered with the Incommon Federation. This can only be edited by the DMPTool Administrator. Contact #{i18n_join(APP_CONFIG['feedback_email_to'])} with any questions.",
+        shib_domain_id_tooltip: "This can only be edited by the DMPTool Administrator. Contact #{i18n_join APP_CONFIG['feedback_email_to']} with any questions.",
+        parent_tooltip: "If you do not see your institution listed in the dropdown list, contact #{i18n_join(APP_CONFIG['feedback_email_to'])}."
       }
     },
     resource_contexts: {
