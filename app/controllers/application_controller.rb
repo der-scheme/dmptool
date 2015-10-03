cf2585c920dc0e9624719de8e4929c4674217038
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2015-10-03 22:16:48 +0200

    Change default_url_options to no longer force the locale.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
     end
 
     def default_url_options(options = {})
-      { locale: I18n.locale }.merge options
+      {locale: nil}.merge options
     end
 
     def current_user
