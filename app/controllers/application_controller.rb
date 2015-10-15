82ab744a200a4cf6371ec34124a279c806c23318
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2015-10-15 22:32:20 +0200

    Implement namespacing for the sortable DSL.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -232,20 +232,25 @@ class ApplicationController < ActionController::Base
     # matches the order scope, or if the ordering is marked as default (which
     # is triggered when the order_scope #param is blank).
 
-    def sortable(attribute, _default = nil, default: false, nested: nil,
-                 inst_var: nil, model: nil, order_scope: :order_scope)
+    def sortable(attribute, _default = nil,
+                 default: false, nested: nil,
+                 inst_var: controller_name.pluralize,
+                 model: controller_name.classify.constantize,
+                 order_scope: :order_scope, namespace: nil)
       fail ArgumentError,
            'expected the 2nd parameter to be one of: :default, nil' unless
         _default.nil? || :default == _default
+
       default ||= _default
+      order_scope = :"#{namespace}:order_scope" if namespace
+      direction = namespace ? :"#{namespace}:direction" : :direction
+
       return unless (params[order_scope] && params[order_scope].to_sym == attribute) ||
                     (default && params[order_scope].blank?)
 
       attribute   = attribute.to_sym
       order_attr  = attribute
-      inst_var    ||= self.class.controller_name.pluralize
-      model       ||= self.class.controller_name.classify.constantize
-      @direction  ||= params[:direction] =~ /desc/i ? 'desc' : 'asc'
+      @direction  ||= params[direction] =~ /desc/i ? 'desc' : 'asc'
 
       collection  = instance_variable_get("@#{inst_var}")
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,16 +5,19 @@ module ApplicationHelper
   #or css class (not yet implemented) associated with the sorting direction
 
   def sortable(column, title = nil,
-               order_scope: :order_scope,
+               order_scope: :order_scope, namespace: nil,
                model: controller_name.classify.constantize)
     fail ArgumentError, 'expected model to be of type ActiveRecord::Base' if
       model && !model.respond_to?(:human_attribute_name)
 
+    order_scope = :"#{namespace}:order_scope" if namespace
+    direction = namespace ? :"#{namespace}:direction" : :direction
+
     title ||= model.human_attribute_name(column) if model
     title ||= t(".#{column}", default: column.titleize)
 
-    css_class = "current #{params[:direction]}" if column == params[order_scope]
-    direction = (column.to_s == params[order_scope] && params[:direction] == 'asc') ? 'desc' : 'asc'
+    css_class = "current #{params[direction]}" if column == params[order_scope]
+    direction = (column.to_s == params[order_scope] && params[direction] == 'asc') ? 'desc' : 'asc'
     link_to title,
             filter_params.merge(order_scope => column, direction: direction),
             {class: css_class}
