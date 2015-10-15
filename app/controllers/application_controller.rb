2b7df8d069ff83a34e080b8f1574cbd3b2f9ba05
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2015-10-15 23:41:40 +0200

    Move the sortable DSL to lib.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include Sortable::Controller
 
   ROLES =
       {   :dmp_admin              => Role::DMP_ADMIN,
@@ -221,53 +222,6 @@ class ApplicationController < ActionController::Base
       if @rt_tree.length == 1 && !@rt_tree.first[1].nil?
         @rt_tree = Hash[ @rt_tree.first[1].map{|i| [i, nil]} ]
       end
-    end
-
-    ##
-    # Declares an attribute sortable.
-    #
-    # The instance variable with the pluralized controller name (i.e.
-    # "resource_contexts" in case of the ResourceContextsController) is treated
-    # as the collection to be sorted and will be sorted if the attribute
-    # matches the order scope, or if the ordering is marked as default (which
-    # is triggered when the order_scope #param is blank).
-
-    def sortable(attribute, _default = nil,
-                 default: false, nested: nil,
-                 inst_var: controller_name.pluralize,
-                 model: controller_name.classify.constantize,
-                 order_scope: :order_scope, namespace: nil)
-      fail ArgumentError,
-           'expected the 2nd parameter to be one of: :default, nil' unless
-        _default.nil? || :default == _default
-
-      default ||= _default
-      order_scope = :"#{namespace}:order_scope" if namespace
-      direction = namespace ? :"#{namespace}:direction" : :direction
-
-      return unless (params[order_scope] && params[order_scope].to_sym == attribute) ||
-                    (default && params[order_scope].blank?)
-
-      attribute   = attribute.to_sym
-      order_attr  = attribute
-      @direction  ||= params[direction] =~ /desc/i ? 'desc' : 'asc'
-
-      collection  = instance_variable_get("@#{inst_var}")
-
-      if nested
-        assoc, assocs = *model_association(model, attribute)
-        collection = collection.joins(assoc)
-        order_attr = "#{assocs}.#{nested}"
-      end
-
-      if block_given?
-        collection = yield collection
-      else
-        collection = collection.order("#{order_attr} #{@direction}")
-      end
-
-      instance_variable_set("@#{inst_var}", collection)
-      nil
     end
 
   private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,5 @@
 module ApplicationHelper
-
-  #to enable column sorting with toggle effects
-  #current asc and current desc classes are for supporting an eventual arrow image
-  #or css class (not yet implemented) associated with the sorting direction
-
-  def sortable(column, title = nil,
-               order_scope: :order_scope, namespace: nil,
-               model: controller_name.classify.constantize)
-    fail ArgumentError, 'expected model to be of type ActiveRecord::Base' if
-      model && !model.respond_to?(:human_attribute_name)
-
-    order_scope = :"#{namespace}:order_scope" if namespace
-    direction = namespace ? :"#{namespace}:direction" : :direction
-
-    title ||= model.human_attribute_name(column) if model
-    title ||= t(".#{column}", default: column.titleize)
-
-    css_class = "current #{params[direction]}" if column == params[order_scope]
-    direction = (column.to_s == params[order_scope] && params[direction] == 'asc') ? 'desc' : 'asc'
-    link_to title,
-            filter_params.merge(order_scope => column, direction: direction),
-            {class: css_class}
-  end
+  include Sortable::Helper
 
   def link_to_add_fields(name, f, association)
     new_object = f.object.send(association).klass.new
