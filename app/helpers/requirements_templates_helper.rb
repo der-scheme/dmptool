897ff6985a3d033278f83dae7717df497e2b66fa
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2015-10-22 15:59:36 +0200

    Remove unused code from requirements_templates_helper.

--- app/helpers/requirements_templates_helper.rb
+++ app/helpers/requirements_templates_helper.rb
@@ -1,14 +1,4 @@
 module RequirementsTemplatesHelper
-  def activate_link_text(requirements_template)
-    requirements_template.active? ?
-        t('globals.template.details.action_deactivate') :
-        t('globals.template.details.action_activate')
-  end
-	def display_status_text(requirements_template)
-    requirements_template.active? ?
-        t('globals.template.details.status_active') :
-        t('globals.template.details.status_inactive')
-  end
   def referer_hash
     @referer_hash ||= session[:page_history][0] || {}
   end
