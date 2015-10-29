3b2ccfe312f27cc456c0d44bfdb1ff983db96ebd
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2015-10-29 13:03:00 +0100

    Fix error if request does not contain the accept_language header.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -240,8 +240,10 @@ class ApplicationController < ActionController::Base
     end
 
     def extract_locale_from_accept_language_header
-      locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first.to_sym
-      locale if Rails.application.config.i18n.available_locales.include?(locale)
+      locale = request.env['HTTP_ACCEPT_LANGUAGE']
+          .try(:scan, /^[a-z]{2}(?:-[A-Z]{2})/)
+          .try(:first).try(:to_sym)
+      locale if locale.in?(Rails.application.config.i18n.available_locales)
     end
 
     ## Stores the current #params in the #session hash.
