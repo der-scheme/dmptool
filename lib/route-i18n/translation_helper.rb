14bfeadbdd048797f918b0f5a1d2408ed063b80a
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2015-11-13 15:52:26 +0100

    Add code for route translations.

--- lib/route-i18n/translation_helper.rb
+++ lib/route-i18n/translation_helper.rb
@@ -2,5 +2,31 @@
 module RouteI18n
   module TranslationHelper
 
+    ##
+    # Defines a method "#{route.name}_text" which returns the localized link
+    # text for each route available.
+    #
+    # I18n keys are composed in the following way:
+    # "routes.controller.action[.format]"
+
+    def self.included(_)
+      Rails.application.routes.routes.each do |route|
+        define_method(:"#{route.name}_text") do |*args, format: :html, **options|
+          fail ArgumentError,
+               "wrong number of arguments (#{args.size} for 0..1)" if
+            args.size > 1
+
+          options[:id] ||= args.first if args.size == 1
+          options[:default] ||= [route.name.titlecase]
+          controller, action = route.defaults.values_at(:controller, :action)
+
+          result = I18n.t(action, scope: [:routes, controller], **options)
+          return result unless result.is_a?(Hash)
+
+          I18n.t(format, scope: [:routes, controller, action], **options)
+        end
+      end
+    end
+
   end
 end
