926ce5f4ff4407215fafbf49cf8d4f0466e5cc77
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2015-11-16 15:59:41 +0100

    Fix translated links behaving unexpectedly.

--- lib/route_i18n/link_helper.rb
+++ lib/route_i18n/link_helper.rb
@@ -7,14 +7,25 @@ module RouteI18n
 
     ##
     # Defines a method "link_to_#{route.name}" which generates a link to
-    # "#{route.name}_path", labelled with the "#{route.name}_text" for each
+    # "#{route.name}_path", labelled with the "#{route.name}_text", for each
     # route available.
 
     def self.included(_)
       Rails.application.routes.routes.each do |route|
-        define_method(:"link_to_#{route.name}") do |url = {},
-                                                    html_options = {}, &body|
+
+        ##
+        # Returns a labelled link to the route in question.
+        #
+        # :call-seq: link_to_route_name(html_options = {}) {|text| …}?
+        # :call-seq: link_to_route_name(url = {}, html_options = {}) {|text| …}?
+
+        define_method(:"link_to_#{route.name}") do |url = nil,
+                                                    html_options = nil, &body|
+          url, html_options = {}, url if url.is_a?(Hash) && html_options.nil?
           url = {id: url.id} if url.is_a? ActiveRecord::Base
+          url ||= {}
+          html_options ||= {}
+
           text = send(:"#{route.name}_text")
 
           link_to route.defaults.merge(url), html_options do
