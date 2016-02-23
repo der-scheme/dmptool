63f34f1da7bb0205d702eb2ae8b6344fdf508105
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2016-02-23 17:39:04 +0100

    Show datepickers only if support for input[type=date] was not detected.
    Includes styles for pseudo elements of webkit's native date picker. There is
    no support on other engines at this time.

--- app/assets/javascripts/plans.js
+++ app/assets/javascripts/plans.js
@@ -1,14 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(function() {
-	$("#plan_submission_deadline.datepicker").datepicker( {
-		showOn: 'button',
-		buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
-		dateFormat: "yy-mm-dd",
-		changeMonth: true,
-		changeYear: true,
-		numberOfMonths: 1
-	});
+	if (!Modernizr.inputtypes.date) {
+		$("#plan_submission_deadline.datepicker").datepicker( {
+			showOn: 'button',
+			buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
+			dateFormat: "yy-mm-dd",
+			changeMonth: true,
+			changeYear: true,
+			numberOfMonths: 1
+		});
+	}
 });
 
 $(function() {
