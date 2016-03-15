b6a6d8f62a7ca5c8d84bbc92fc3475376b1fb98b
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2016-03-15 18:28:26 +0100

    Fix secret_key_base vulnerability which allows an attacker to generate arbitrary sessions.
    The secret_key_base is used in cookie encryption. If someone were to use the
    default config, their secret_key_base would be known to the public.
    This fix ensures that the secret_key_base is regenerated on each server boot,
    unless in development mode.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,6 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Dmptool2::Application.config.secret_key_base = '9555787b8643f7b2a5cc1896078b81767421f48f39b3f6fecc6362dfbcf6cba5f26341b3e4b689d5788b3ad21be2d903608012243428ea9b4f2cb511a1e21a27'
+Dmptool2::Application.config.secret_key_base = ENV['RAILS_ENV'] == 'development' ?
+    '9555787b8643f7b2a5cc1896078b81767421f48f39b3f6fecc6362dfbcf6cba5f26341b3e4b689d5788b3ad21be2d903608012243428ea9b4f2cb511a1e21a27' :
+    `rake secret`
