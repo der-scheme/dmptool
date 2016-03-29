2271663721cdf406a3bd0e8f6aa615cb6d928a88
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2016-03-29 17:21:09 +0200

    Fix typo causing reinitialization of Layout code on every render.

--- lib/layout.rb
+++ lib/layout.rb
@@ -1,10 +1,10 @@
 
 module Layout
   def self.header
-    @@header = Header.new(Rails.configuration.layout[:header])
+    @@header ||= Header.new(Rails.configuration.layout[:header])
   end
 
   def self.footer
-    @@footer = Footer.new(Rails.configuration.layout[:footer])
+    @@footer ||= Footer.new(Rails.configuration.layout[:footer])
   end
 end
