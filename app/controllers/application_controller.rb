afaa583d0731629dfcba0b9ee536fe7b78247501
Sebastian Dufner <dufners@informatik.uni-freiburg.de>
2016-04-13 19:43:38 +0200

    Revert "Fix our implementation of default_url_options always returning a new object,"
    This reverts commit c45f69559b5f7534cb598335c9571fef15691113.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,11 +23,12 @@ class ApplicationController < ActionController::Base
   protected
 
     def set_locale
-      locale = params[:locale] ||
+      I18n.locale = params[:locale] ||
           extract_locale_from_accept_language_header || I18n.default_locale
+    end
 
-      default_url_options[:locale] = locale
-      I18n.locale = locale
+    def default_url_options(options = {})
+      {locale: params[:locale]}.merge options
     end
 
     def current_user
